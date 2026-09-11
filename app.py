bed6465dc111d0d0ed550ce30048b9302eeba356
alicate99x <lucasrafael020688@gmail.com>
2026-09-10 21:49:03 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def inicio():
+    return render_template("index.html")
+
+
+@app.route("/calculadora")
+def calculadora():
+    return render_template("calculadora.html")
+
+
+@app.route("/resultado")
+def resultado():
+    return render_template("resultado.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
